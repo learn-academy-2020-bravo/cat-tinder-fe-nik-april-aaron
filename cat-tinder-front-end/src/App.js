77e5e8d7d5bc39a03d9cc8349542c2a09bfcdca3
April Rieger <63515648+aprilrieger@users.noreply.github.com>
2020-06-16 17:40:27 -0700

    created Index Component -- 3rd tutorial in frontend Cat Tinder Series

--- cat-tinder-front-end/src/App.js
+++ cat-tinder-front-end/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
-
+import cats from './cats'
 import Header from './components/Header'
 import CatIndex from './pages/CatIndex'
 
-const App = () => {
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      allCats: cats
+    }
+  }
+  render(){
     return(
-      <React.Fragment>
         <Router>
         <Header />
           <Switch>
-            <Route exact path="/" component={ CatIndex } />
+            <Route exact path="/" render={ (props) => <CatIndex cats={ this.state.allCats } /> } />
           </Switch>
         </Router>
-      </React.Fragment>
     )
+  }
 }
+
 export default App
--- cat-tinder-front-end/src/pages/CatIndex.js
+++ cat-tinder-front-end/src/pages/CatIndex.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-
-
+import { ListGroup, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 class CatIndex extends Component{
     render(){
         return(
         <React.Fragment>
-        <h1>I Am A Cat</h1>
+            { this.props.cats.map((cat, index) => {
+            return(
+                <ListGroup key={ index }>
+                <h4>{ cat.name }</h4>
+                <small>{ cat.age } - { cat.enjoys }</small>
+                </ListGroup>
+                )
+            })}
         </React.Fragment>
         )
     }
